7d460509e6aa2cf1c8dc19a50298b37f0643c2d6
Paul McFedries <mail@mcfedries.com>
2023-02-19 11:37:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>index.php</title>
+    </head>
+    <body>
+        <h1>Hello index.php World!</h1>
+    </body>
+</html>
